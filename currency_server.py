ed1d45af794eb938f52ac7bd424bf40a711c31f8
Dima Ovchinnikov <ovchinnikov.dm16@physics.msu.ru>
2022-06-06 11:05:25 +0300

    doc strings with requests examples

--- currency_server.py
+++ currency_server.py
@@ -20,11 +20,21 @@ def welcome_page():
 
 @app.get("/minmax")
 def api_minmax(request: CurrencyRequest):
+    """'r = requests.get(
+    "http://127.0.0.1:8000/minmax",
+    json={"currency": "RUB",
+          "start_date": "03.05.2022",
+          "end_date": "01.06.2022"})"""
     return minmax([request.currency, request.start_date, request.end_date])
 
 
 @app.get("/list")
 def api_list_notes(request: CurrencyRequest):
+    """'r = requests.get(
+        "http://127.0.0.1:8000/list",
+        json={"currency": "RUB",
+              "start_date": "03.05.2022",
+              "end_date": "01.06.2022"})"""
     temp = list_notes([request.currency, request.start_date, request.end_date])
 
     response = {"count": len(temp)}
